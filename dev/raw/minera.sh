883c910106c553c8803a1ad544892da8e76a0a12
Sultan <138652076+sulfar0@users.noreply.github.com>
2026-01-28 21:15:21 +0700

    update

--- dev/raw/minera.sh
+++ dev/raw/minera.sh
@@ -4,19 +4,22 @@ sed -e
 
 #--[1] validating package
 #--[1][1] validating package apache
-
 #--[1](1) validating package apache conditional true
-
 #--[1](1) validating package apache conditional false
-
 #--[1][2] validating package mariadb
 #--[1](2) validating package mariadb conditional true
 #--[1](2) validating package mariadb conditional false
 #--[2] database configuration
 #--[3] apache configuration
-#--[4] list wordpress
-#--[4][1]
-#--[5] 
+#--[4] wordpress
+#--[4][1] wordpress list version url
+#--[4](2) download wordpress via url conditional true
+#--[4](2) download wordpress via url conditional false
+#--[5] php adjusting with the wordpress selection
+#--[5](1) validating php version conditional true
+#--[5](1) validating php version conditional false
+#--[6] congrats screen
+
 
 
 
